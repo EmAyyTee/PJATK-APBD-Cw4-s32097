fd07eaab138dbbb465e4454026e30adebb5c23b5
EmAyyTee <HanzoSwitchMail@gmail.com>
2026-05-25 22:08:10 +0200

    Added EF Core migration

--- PcComponentsApi/Program.cs
+++ PcComponentsApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PcComponentsApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
