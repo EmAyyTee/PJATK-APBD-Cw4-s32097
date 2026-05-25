9144319bc7643e57c33fecb5162ffbcb4a8a4249
EmAyyTee <HanzoSwitchMail@gmail.com>
2026-05-25 21:54:13 +0200

    First commit after making the EF core models

---
+++ PcComponentsApi/Models/Component.cs
@@ -0,0 +1,20 @@
+﻿namespace PcComponentsApi.Models;
+
+public class Component
+{
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int ManufacturerId { get; set; }
+
+    public int TypeId { get; set; }
+
+    public ComponentManufacturer Manufacturer { get; set; } = null!;
+
+    public ComponentType Type { get; set; } = null!;
+
+    public ICollection<PcComponent> PcComponents { get; set; } = new List<PcComponent>();
+}
